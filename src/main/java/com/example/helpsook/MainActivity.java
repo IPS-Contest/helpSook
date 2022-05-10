f60e3ca439101d07d1e5e5cd56e39572591cb7f3
Nayeon-Ryu <2014027@sookmyung.ac.kr>
2022-05-10 23:12:44 +0900

    Base Code Upload

---
+++ src/main/java/com/example/helpsook/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helpsook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.se.omapi.Session.*;
+
+import java.util.logging.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
